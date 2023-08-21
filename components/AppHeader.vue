77be4aea7cf8949df1582720a0206a53402d670a
Shota-Kizaki <104074879+Shota-Kizaki@users.noreply.github.com>
2023-08-21 04:52:02 +0000

    a

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,17 +1,18 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
+        <a class="navbar-brand" href="#">財務会計</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">財務会計</a>
+          <a class="navbar-brand" href="#">ホーム</a>
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <nuxt-link to="/index" >ホーム</nuxt-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <nuxt-link to="/link" >リンク</nuxt-link>
             </li>
             <li class="nav-item">
               <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
-    <h1>Hello World</h1>
+    <h1>各種入力</h1>
+  </div>
+  <div class="container">
+    <h1>各種出力</h1>
   </div>
 </template>
