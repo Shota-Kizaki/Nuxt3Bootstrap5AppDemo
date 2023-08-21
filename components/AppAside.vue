8ea535adc6b3a0f8cd4df31560584c5b2c9426a2
Shota-Kizaki <104074879+Shota-Kizaki@users.noreply.github.com>
2023-08-21 06:00:43 +0000

    いｍｇ

--- components/AppAside.vue
+++ components/AppAside.vue
@@ -1,3 +1,30 @@
 <template>
-
+    <aside class="app-aside">
+      <div class="chat-box">
+        <div class="message-list">
+          <div v-for="message in messages" :key="message.id" class="message">
+            {{ message.text }}
+          </div>
+        </div>
+        <div class="input-box">
+          <input v-model="newMessage" @keydown.enter="sendMessage" placeholder="Type your message..." />
+        </div>
+      </div>
+    </aside>
 </template>
+  
+<script setup>
+import { ref } from 'vue';
+  const messages = ref([]);
+  const newMessage = ref('');
+  
+  const sendMessage = () => {
+    if (newMessage.trim() === '') return;
+    
+    messages.value.push({ id: Date.now(), text: newMessage });
+    newMessage.value = '';
+  
+    // OpenAI APIの呼び出しと返信の処理を追加
+    // ここでOpenAIのAPIを使用してメッセージの返信を取得する
+  };
+  </script>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,33 +1,30 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <nuxt-link to="/">
-          <img src="/img/logo.png" width="100" height="auto" class="img-fluid"/>
-        </nuxt-link>
-        <nuxt-link to="/">
-          <a class="navbar-brand" href="#">財務会計</a>
-        </nuxt-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <nuxt-link to="/" >ホーム</nuxt-link>
-            </li>
-            <li class="nav-item">
-              <nuxt-link to="/link" >リンク</nuxt-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <nuxt-link to="/" class="navbar-brand">
+        <img src="/img/logo.png" alt="Logo" width="100" height="auto" class="img-fluid" />
+        財務会計
+      </nuxt-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <nuxt-link to="/" class="nav-link">ホーム</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/link" class="nav-link">リンク</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
-    </nav>
+    </div>
+  </nav>
   </template>
-  
